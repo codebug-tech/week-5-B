e9818abffec09f5db53f1a033a67c23d3f51cdf4
mcarthurgill <mcarthur.gill@gmail.com>
2017-12-10 21:42:45 -0600

    refreshing

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log(10 < 150)
 // my guess: true
 
 
-// Go look in the developer tools console and notice that `true` is printed below the sentence `10 is less than 150`. After you finish each comparison, check the developer tools console to make sure it printed what you predicted. 
+// Go look in the developer tools console and notice that `true` is printed below the sentence `10 is less than 150`. After you finish each comparison, refresh your Chrome page and check the developer tools console to make sure it printed what you predicted. 
 /////////////////
 
 
